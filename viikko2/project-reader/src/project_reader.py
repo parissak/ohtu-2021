8ce5667cdb4495a9302e6e5379c98052c94a2734
parissak <sapak@protonmail.com>
2021-01-19 15:50:46 +0200

    solution for second week's no 2

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,22 @@
+from urllib import request
+from project import Project
+
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        parsed_toml = toml.loads(content)
+        name = parsed_toml['tool']['poetry']['name']
+        description = parsed_toml['tool']['poetry']['description']
+        dependencies = parsed_toml['tool']['poetry']['dependencies']
+        dev_dependencies = parsed_toml['tool']['poetry']['dev-dependencies']
+        
+        return Project(name, description, dependencies, dev_dependencies)
